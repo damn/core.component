aca2e729877804589480ea5d7840659ba30b4d3a
Michael Sappler <michaelsappler@gmail.com>
2023-07-28 17:26:51 +0200

    Remove too much magic code, update README

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # x.x
 
-A Clojure Entity Component System working on plain atoms, maps and keywords.
+A Clojure [Entity Component System](https://en.wikipedia.org/wiki/Entity_component_system) working on plain atoms, maps and keywords.
+
+> Entity component system (ECS) is a software architectural pattern mostly used in video game development for the representation of game world objects. An ECS comprises entities composed from components of data, with systems which operate on entities' components.
+>
+> ECS follows the principle of composition over inheritance, meaning that every entity is defined not by a type hierarchy, but by the components that are associated with it. Systems act globally over all entities which have the required components.
+
+# Rationale
+
+Because game state is stored in different atoms for each entity, we have to make use with side effects.
+But for functions which only handle local component/entity state we want to make them purely functional.
+x.x allows us to separate both functions with side effects and pure functions cleanly.
+
+# Future work
+
+* Transients in the reduce fns
+
+* Datomic? (not walk all entity keys all the time but query directly for components)
 
 # Installation
 
@@ -9,40 +25,58 @@ A Clojure Entity Component System working on plain atoms, maps and keywords.
 [com.github.damn/x.x "x"]
 ```
 
+## Glossary
+
+Abbreviation | Meaning | Datatype
+----- | ----    | ----
+ c   | (component) type  | keyword
+ v   | (component) value | anything
+ e   | entity            | map
+ r   | entity-reference  | atom
+ sys | system            | multimethod
+
+Note that the main systm works just with plain maps
+and using atoms and systems with side-effects do not need to be used.
+
 ## Example
 
 ``` clojure
 (require '[x.x :as x])
 
-(x/defx create)
-; => [#'user/create [#'user/create-c #'user/create-e #'user/create!]]
+(x/defsystem create   [c v] v) ; just takes value and returns new value
+(x/defsystem create-e [c e] e) ; just takes entity and returns new entity
+(x/defsystem create!  [c r]) ; for side-effects
 
-(x/extend-c :a
-  (create-c [v] (inc v))
-  (create-e [v e] (update e :foo dec))
-  (create! [r]
+(x/extend-component :a
+  (create [_ v] (inc v))
+  (create-e [_ e] (update e :foo dec))
+  (create! [_ r]
     (println "CREATE A !")
     (swap! r assoc-in [:fooz :bar :baz] 3)))
 
-(x/extend-c :b
-  (create-e [v e] (assoc e :babaz :cool))
-  (create! [r] (println "B says hi")))
+(x/extend-component :b
+  (create-e [_ e] (assoc e :babaz :cool))
+  (create! [_ r] (println "B says hi")))
 
-(x/!x! create (atom {:a 0 :b 10 :foo 10}))
+(def create-systems [create create-e create!])
+
+(x/!x! create-systems (atom {:a 0 :b 10 :foo 10}))
 ; =>
 ; CREATE A !
 ; B says hi
 ; {:a 1, :b 10, :foo 9, :babaz :cool, :fooz {:bar {:baz 3}}}
 
-(x/defx tick)
+; using extra params with defsystem
+
+(x/defsystem tick [component value delta] value)
 ; => [#'user/tick [#'user/tick-c #'user/tick-e #'user/tick!]]
 
-(x/extend-c :a
-  (tick-c [v delta]
+(x/extend-component :a
+  (tick [_ v delta]
     (update v :counter + delta)))
 
-(x/!x! tick (atom {:a {:counter 0}}) 10)
-; => {:a {:counter 10}}
+(reduce-v tick {:a {:counter 0}} 10)
+; {:a {:counter 10}}
 ```
 
 ## License
@@ -59,3 +93,4 @@ Public License, v. 2.0 are satisfied: GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or (at your
 option) any later version, with the GNU Classpath Exception which is available
 at https://www.gnu.org/software/classpath/license.html.
+
--- src/x/x.clj
+++ src/x/x.clj
@@ -1,38 +1,76 @@
 (ns x.x)
 
-(defmacro defcs [sym] `(defmulti ~sym (fn [c# & _#] c#)))
-
-(defn def-def [cs suffix]
-  (case suffix
-    "-c" (defmethod cs :default [c v   & _] v)
-    "-e" (defmethod cs :default [c v e & _] e)
-    "!"  (defmethod cs :default [c r   & _]  )))
-
-(defmacro defx [sym]
-  (let [cs `(vector
-             ~@(for [s ["-c" "-e" "!"]
-                     :let [cs (symbol (str (name sym) s))]]
-                 `(do (defcs ~cs) (def-def ~cs ~s) (var ~cs))))]
-    `(let [cs# ~cs]
-       [(def ~sym cs#) cs#])))
-
-(defn reduce-apply-c [cs e & args] (reduce (fn [e c] (update e c #(apply cs c % args))) e (keys e)))
-(defn reduce-apply-e [cs e & args] (reduce (fn [e c] (apply cs c (c e) e args))         e (keys e)))
-(defn doseq-apply-r! [cs r & args] (doseq [[c v] @r] (apply cs c r args))) ; do not pass v, might change
-
-(defn !x! [[cs-c cs-e cs-r] r & args]
-   (let [e (apply reduce-apply-c cs-c @r args)
-         e (apply reduce-apply-e cs-e  e args)]
-     (reset! r e)
-     (apply doseq-apply-r! cs-r r args)
-     @r))
-
-(defn extend-cs [cs c f] (defmethod cs c [_ & args] (apply f args)))
-
-(defmacro extend-c [c & cs-impls]
+(defmacro defsystem
+  {:arglists '([name [params*] default-return-value?])}
+  [sys-name params & default-return-value]
+  (when (zero? (count params))
+    (throw (IllegalArgumentException. "Requires at least 1 param for dispatching.")))
   `(do
-    ~@(for [[cs & fn-b] cs-impls
-            :let [fn-n (symbol (str (name c) "-" (name cs)))
-                  fn-f `(fn ~fn-n ~@fn-b)]]
-        `(extend-cs ~cs ~c ~fn-f))
+    (defmulti ~(vary-meta sys-name assoc :params (list 'quote params))
+      (fn [~@params] ~(first params)))
+    (defmethod ~sys-name :default ~params ~@default-return-value)
+    (var ~sys-name)))
+
+(defmacro extend-component [c & sys-impls]
+  `(do
+    ~@(for [[sys & fn-body] sys-impls
+            :let [sys-var (resolve sys)
+                  sys-params (:params (meta sys-var))
+                  fn-params (first fn-body)]]
+        (do
+         (when-not (= (count sys-params) (count fn-params))
+           (throw (IllegalArgumentException.
+                   (str sys-var " requires " (count sys-params) " args: " sys-params "."
+                                   " Given " (count fn-params)  " args: " fn-params))))
+         `(defmethod ~sys ~c ~@fn-body)))
     ~c))
+
+(defn reduce-v
+  "Applies a system with params [c v & more] on an entity e, updating v for every c."
+  [sys e & args]
+  (reduce (fn [e c]
+            (update e c #(apply sys c % args)))
+          e
+          (keys e)))
+
+(defn reduce-e
+  "Applies a system with params [c e & more] on an entity e, updating e for every c."
+  [sys e & args]
+  (reduce (fn [e c]
+            (apply sys c e args))
+          e
+          (keys e)))
+
+(defn doseq-r!
+  "Doseq's over (keys @r), calling a system with params [c r & more]"
+  [sys r & args]
+  (doseq [c (keys @r)]
+    (apply sys c r args)))
+
+(defn !x! [[sys-v sys-e sys-r] r & args]
+  (let [e (apply reduce-v sys-v @r args)
+        e (apply reduce-e sys-e  e args)]
+    (reset! r e)
+    (apply doseq-r! sys-r r args)
+    r))
+
+; this hurts the rule of clarity & explicity -> too much magic?
+; also hidden functions which grep cannot find...
+; rather declare 3 systems explicitly, there are not so many systems in a game usually ?
+(comment
+ (defmacro defsystem-v [name]
+   `(defsystem ~name [~'c ~'v] ~'v))
+ ; defsystem-e, defsystem-r, ...
+
+ (macroexpand-1 '(defsystem-v create))
+
+ (defmacro defsystems [sys-name]
+   (let [suffix #(symbol (str (name sys-name) %))
+         ssys (suffix "-systems")
+         vsys sys-name
+         esys (suffix "-e")
+         rsys (suffix "-!")]
+     `(let [systems# [(defsystem ~vsys [~'c ~'v] ~'v)
+                      (defsystem ~esys [~'c ~'e] ~'e)
+                      (defsystem ~rsys [~'c ~'r])]]
+        [(def ~ssys systems#) systems#]))))
