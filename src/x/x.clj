5ba4941f852b4c85f359f317cc5523519a3df995
Michael Sappler <michaelsappler@gmail.com>
2023-07-29 11:09:05 +0200

    Refactoring

--- src/x/x.clj
+++ src/x/x.clj
@@ -30,47 +30,32 @@
          `(defmethod ~sys ~c ~@fn-body)))
     ~c))
 
-(defn reduce-vs [f m]
+(defn update-map [f m]
   (persistent!
    (reduce-kv (fn [new-map k v]
                 (assoc! new-map k (f k v)))
               (transient {})
               m)))
 
-(defn reduce-v [sys e & args] (reduce-vs (fn [c v]  (apply sys c v args)) e))
-(defn doseq-e! [sys e & args] (doseq [c (keys e)]   (apply sys c e args)))
-(defn doseq-r! [sys r & args] (doseq [c (keys @r)]  (apply sys c r args)))
+; TODO I could check (keys (methods sys)) and only update as needed
+; transient works with 'clojure.core/update' ? https://groups.google.com/g/clojure/c/h5ks9KDHQmE
+; => perf tests
 
-(defn !x! [[sys-v sys-r] r & args] ; TODO just x! or apply!
-  (let [e (apply reduce-v sys-v @r args)]
+(defn apply-system [sys e & args]
+  (update-map (fn [c v] (apply sys c v args))
+              e))
+
+(defn apply-system! [sys r & args]
+  (doseq [c (keys @r)]
+    (apply sys c r args)))
+
+(defn apply-systems! [[sys-v sys-r] r & args] ; TODO just x! or apply!
+  (let [e (apply apply-system sys-v @r args)]
     (reset! r e)
-    (apply doseq-r! sys-r r args)
+    (apply apply-system! sys-r r args)
     r))
 
 (defmacro defsystems [sys-name [vsys rsys] & {:keys [extra-params]}]
   `(let [systems# [(defsystem ~vsys [~'c ~'v ~@extra-params] ~'v)
                    (defsystem ~rsys [~'c ~'r ~@extra-params])]]
      [(def ~sys-name systems#) systems#]))
-
-#_(defn intern-clojure []
-  (in-ns 'clojure.core)
-  (require 'potemkin.namespaces)
-
-  (let [nmspace "x.x"
-        publics (butlast (keys (ns-publics (symbol nmspace))))]
-
-    (println "publics: " (count publics))
-    (println publics)
-
-    (doseq [sym publics]
-      (println sym)
-
-      ; TODO warns/throws on refresh-all
-      (when (ns-resolve 'clojure.core sym)
-        (throw (IllegalArgumentException. (str "sym cannot be interned: " (name sym))))))
-
-    (let [syms (map #(symbol nmspace (name %)) publics)]
-      (eval
-       `(potemkin.namespaces/import-vars ~@syms)))
-
-    (in-ns (symbol nmspace))))
