74d6309222c1257edbde8459a7be38c5cd7f00c4
Michael Sappler <michaelsappler@gmail.com>
2024-07-29 07:40:54 +0530

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :plugins [[lein-codox "0.10.8"]] ; TODO dev only ...
-  :codox {:source-uri "https://github.com/damn/x.x/blob/main/{filepath}#L{line}"}
-  :repl-options {:init-ns x.x})
+  :codox {:source-uri "https://github.com/damn/core.component/blob/main/{filepath}#L{line}"}
+  :repl-options {:init-ns core.component})
